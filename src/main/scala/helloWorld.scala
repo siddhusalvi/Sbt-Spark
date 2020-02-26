3dd513e772dbc09bb009668aef3a4a11386fb617
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-26 12:56:48 +0530

    Added :Testing programs

--- src/main/scala/helloWorld.scala
+++
@@ -1,5 +0,0 @@
-package sbtspark
-import org.apache.spark.sql
-object helloWorld extends App{
-  println("hello world this is awesome.")
-}
---
+++ src/test/scala/sbtspark/FrameMakerTest.scala
@@ -0,0 +1,68 @@
+package sbtspark
+
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.junit.Test
+
+class FrameMakerTest {
+  @Test def test1 {
+    val data = List(
+      Row("kartik", 90, "Ml"),
+      Row("gaja", 85, "kotlin"),
+      Row("mayank", 70, "automation")
+    )
+
+    val schema = StructType(
+      List(
+        StructField("name", StringType, true),
+        StructField("performance", IntegerType, true),
+        StructField("technology", StringType, true)
+      )
+    )
+    val appName = "Scala example"
+    val master = "local"
+    val spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
+    val rdd = spark.sparkContext.parallelize(data)
+    //Creating date frame by local session
+    val expected_frame = spark.createDataFrame(rdd, schema)
+
+    //getting data frame from FrameMaker class
+    var frameMaker = new FrameMaker
+    val frame1 = frameMaker.toFrame(schema, data)
+
+    assert(expected_frame.except(frame1).count() == 0)
+  }
+
+  @Test def test2 {
+    val data = List(
+      Row("kartik", 90, "Ml"),
+      Row("gaja", 85, "kotlin"),
+      Row("mayank", 70, "aautomation")
+    )
+
+    val data1 = List(
+      Row("kartik", 90, "Ml"),
+      Row("gaja", 85, "kotlin"),
+      Row("mayank", 70, "automation")
+    )
+
+    val schema = StructType(
+      List(
+        StructField("name", StringType, true),
+        StructField("performance", IntegerType, true),
+        StructField("technology", StringType, true)
+      )
+    )
+    val appName = "Scala example"
+    val master = "local"
+    val spark = SparkSession.builder.appName(appName).master(master).getOrCreate()
+    val rdd = spark.sparkContext.parallelize(data)
+    //Creating date frame by local session
+    val expected_frame = spark.createDataFrame(rdd, schema)
+
+    //getting data frame from FrameMaker class
+    var frameMaker = new FrameMaker
+    val frame1 = frameMaker.toFrame(schema, data1)
+    assert(expected_frame.except(frame1).count() == 0)
+  }
+}
