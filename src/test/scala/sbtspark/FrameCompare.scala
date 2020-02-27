1d9de183a08aa66d3cac0925df43dc3bed0efc0d
siddhusalvi <siddheshsalvi525@gmail.com>
2020-02-27 18:10:06 +0530

    ADDED: Class to compare data frame

--- src/test/scala/sbtspark/FrameCompare.scala
+++ src/test/scala/sbtspark/FrameCompare.scala
@@ -1,5 +1,14 @@
 package sbtspark
 
-class FrameCompare {
+import org.apache.spark.sql.DataFrame
 
+class FrameCompare {
+//Function two compare frames
+  def areEqual(frame1: DataFrame, frame2: DataFrame): Boolean = {
+    if (frame1.except(frame2).count() == 0) {
+      true
+    } else {
+      false
+    }
+  }
 }
